8584aa07f3501f147a600c8b1a19b4d98d988bed
Dulan666 <dulanhewage2@gmail.com>
2022-10-12 22:35:47 +0530

    initial commit. add PaymentForm.vue component. install sass npm package.

---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="payment-form-wrapper">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.payment-form-wrapper {
+  h1 {
+    color: red;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PaymentForm from "../components/PaymentForm.vue";
+</script>
+
+<template>
+  <main>
+    <PaymentForm msg="Hello"/>
+  </main>
+</template>
